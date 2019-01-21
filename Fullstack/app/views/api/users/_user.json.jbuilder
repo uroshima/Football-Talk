569d8acdc7e666f64195573addb8650d00b93a6e
urosh <urosh.stojkovikj@gmail.com>
2019-01-21 15:15:35 -0800

    added new views to users

--- Fullstack/app/views/api/users/_user.json.jbuilder
+++ Fullstack/app/views/api/users/_user.json.jbuilder
@@ -1,1 +1,9 @@
-json.extract! user, :id, :email
+json.extract! user, :id, :username, :email, :biography
+json.day user.created_at.day
+json.month user.created_at.month
+json.year user.created_at.year
+json.currentUserFollows @currentUserFollows
+
+if user.avatar.attached?
+  json.avatar url_for(user.avatar)
+end
--- Fullstack/db/schema.rb
+++ Fullstack/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_162908) do
+ActiveRecord::Schema.define(version: 2019_01_21_230138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
