acd1b0c59f56d22f56d4a9d11a9cdd374d0b9f25
urosh <urosh.stojkovikj@gmail.com>
2019-01-21 13:53:16 -0800

    created methods in the story model

--- Fullstack/app/models/story.rb
+++ Fullstack/app/models/story.rb
@@ -1,10 +1,52 @@
 class Story < ApplicationRecord
   validates :title, uniqueness: true
-  validates :title, :content, :subtitle, presence: true
+  validates :title, :author_id, :content, :subtitle, presence: true
 
   belongs_to :author,
+  primary_key: :id,
   foreign_key: :author_id,
   class_name: :User
 
+  has_many :responses,
+  primary_key: :id,
+  foreign_key: :story_id,
+  class_name: :Response
+
+  has_many :claps,
+  as: :clappable
+
   has_one_attached :photo
+
+  # methods
+
+    def read_time
+      word_count = self.content.split(' ').length
+      minutes = word_count / 140
+      if minutes < 1
+        "< 1 minute read"
+      else
+        "#{minutes} min read"
+      end
+    end
+
+  def date
+    monthShort = [
+      "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ]
+
+    monthLong= [
+      "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ]
+
+    year = self.created_at.year
+    month = monthShort[self.created_at.month]
+    day = self.created_at.day
+    return "#{month} #{day}"
+  end
+
+  def totalClaps
+    self.claps.sum(:clap_count)
+  end
 end
+
+
